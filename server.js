ca1cc30d4f72934cf26b50df97431632d6983c57
kelvin akunwa <kelvinakunwa101@gmail.com>
2026-05-21 06:44:11 +0100

    fix mongoose connection options

--- server.js
+++ server.js
@@ -67,10 +67,7 @@ app.use((err, req, res, next) => {
 
 /* ---------------- DATABASE CONNECTION ---------------- */
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => {
     console.log("MongoDB connection error:", err);
